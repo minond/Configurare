142183d825b78e456627c26042ad67ed2f333cba
Marcos Minond <minond.marcos@gmail.com>
2013-06-24 00:45:46 -0600

    adding base configuration class

---
+++ src/Efficio/Configurare/Configuration.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Efficio\Configurare;
+
+/**
+ * project configuration reader and writer
+ */
+class Configuration
+{
+    /**
+     * configuration file formats
+     */
+    const JSON = '.json';
+    const YAML = '.yml;
+}
---
+++ tests/Efficio/Configurare/ConfigurationTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Efficio\Tests\Configurare;
+
+use Efficio\Configurare\Configuration
+use PHPUnit_Framework_TestCase;
+
+class ConfigurationTest extends PHPUnit_Framework_TestCase
+{
+}
