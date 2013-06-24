1f88088ffef9dce6371220a1cc3156e7119e99d7
Marcos Minond <minond.marcos@gmail.com>
2013-06-24 01:16:45 -0600

    adding configuration path parsing functions

--- src/Efficio/Configurare/Configuration.php
+++ src/Efficio/Configurare/Configuration.php
@@ -2,14 +2,48 @@
 
 namespace Efficio\Configurare;
 
+use Efficio\Cache\Caching;
+
 /**
  * project configuration reader and writer
  */
 class Configuration
 {
+    use Caching;
+
     /**
-     * configuration file formats
+     * configuration file formats. defaults to yaml
      */
     const JSON = '.json';
-    const YAML = '.yml;
+    const YAML = '.yml';
+
+    /**
+     * configuration path delimeter
+     */
+    const DELIM = ':';
+
+    /**
+     * configuration file format
+     */
+    private $format = self::YAML;
+
+    /**
+     * get the file name from a configuration path
+     * @param string $path
+     * @return string
+     */
+    public static function getFileName($path)
+    {
+        return array_shift(explode(self::DELIM, $path, 2));
+    }
+
+    /**
+     * get the configuration path
+     * @param string $path
+     * @return array
+     */
+    public static function getConfPath($path)
+    {
+        return array_slice(explode(self::DELIM, $path), 1);
+    }
 }
--- tests/Efficio/Configurare/ConfigurationTest.php
+++ tests/Efficio/Configurare/ConfigurationTest.php
@@ -2,9 +2,38 @@
 
 namespace Efficio\Tests\Configurare;
 
-use Efficio\Configurare\Configuration
+use Efficio\Configurare\Configuration;
 use PHPUnit_Framework_TestCase;
 
 class ConfigurationTest extends PHPUnit_Framework_TestCase
 {
+    public function testNewConfigurationClassesCanBeCreated()
+    {
+        $conf = new Configuration;
+        $this->assertTrue($conf instanceof Configuration);
+    }
+
+    public function testFileNamesCanBeParsedFromConfigurationPath()
+    {
+        $this->assertEquals('config/project',
+            Configuration::getFileName('config/project:test:users'));
+    }
+
+    public function testFileNamesCanStillBeParsedWhenThereIsNoConfigurationPath()
+    {
+        $this->assertEquals('config/project',
+            Configuration::getFileName('config/project'));
+    }
+
+    public function testConfigurationPathsCanBeParsed()
+    {
+        $this->assertEquals(['test', 'users', 'first'],
+            Configuration::getConfPath('config/project:test:users:first'));
+    }
+
+    public function testPassingNoConfigurationPathReturnsAnEmptyArray()
+    {
+        $this->assertEquals([],
+            Configuration::getConfPath('config/project'));
+    }
 }
